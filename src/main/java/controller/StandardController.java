1b33b372a436b162d80c319edc745f447bdf4b4b
xuwenqing <qing18363031040>
2016-06-29 11:24:04 +0800

    ajax

--- src/main/java/controller/StandardController.java
+++ src/main/java/controller/StandardController.java
@@ -47,26 +47,32 @@ public class StandardController extends BaseController {
         return null;
     }
 
-    @RequestMapping(value = "/query", method = RequestMethod.GET)
-    public @ResponseBody ResponsePackDto queryquery(
-            @RequestParam(value = "start", defaultValue = "0") int start,
-            @RequestParam(value = "limit", defaultValue = "2") int limit,
-            @RequestParam(value = "sortby", required = false, defaultValue = "publishDate") String sortby,
-            @RequestParam(value = "order", required = false, defaultValue = "desc") String order,
-            @RequestParam(value = "name",required = false) String name,
-            @RequestParam(value = "keywords",required = false) String keywords) {
-
-        StandardCondition condition = new StandardCondition();
-        condition.setStart(start);
-        condition.setLimit(limit);
-        condition.setSortby(sortby);
-        condition.setOrder(order);
-        if(name != null)
-            condition.setName(name);
-        if(keywords != null)
-            condition.setKeywords(keywords);
-        return null;
+    @RequestMapping(value = "/query", method = RequestMethod.POST)
+    public @ResponseBody ResponsePackDto query(@RequestBody StandardCondition condition) {
+        System.out.println(condition);
+        return new ResponsePackDto(condition);
     }
+
+//    @RequestMapping(value = "/query", method = RequestMethod.POST)
+//    public @ResponseBody ResponsePackDto queryquery(
+//            @RequestParam(value = "start", defaultValue = "0") int start,
+//            @RequestParam(value = "limit", defaultValue = "2") int limit,
+//            @RequestParam(value = "sortby", required = false, defaultValue = "publishDate") String sortby,
+//            @RequestParam(value = "order", required = false, defaultValue = "desc") String order,
+//            @RequestParam(value = "name",required = false) String name,
+//            @RequestParam(value = "keywords",required = false) String keywords) {
+//
+//        StandardCondition condition = new StandardCondition();
+//        condition.setStart(start);
+//        condition.setLimit(limit);
+//        condition.setSortby(sortby);
+//        condition.setOrder(order);
+//        if(name != null)
+//            condition.setName(name);
+//        if(keywords != null)
+//            condition.setKeywords(keywords);
+//        return new ResponsePackDto(condition);
+//    }
 
 
 
