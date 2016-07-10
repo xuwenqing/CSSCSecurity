06e4a814a7d5d5f1180b874f011c5eef9d510b62
huchao <1083763917@qq.com>
2016-07-10 23:56:55 +0800

    用户编辑获取角色列表

---
+++ src/main/webapp/WEB-INF/static/admin/handle/role/role.js
@@ -0,0 +1,55 @@
+/**
+ * Created by huchao on 2016/7/10.
+ */
+Entities.Role = (function(Backbone, Entities,_) {
+    var base = 'http://localhost:8080';
+    var API_SAVE =base+'/role/add';//添加角色
+    var API_EDIT = base+'/role/edit';//编辑角色
+    var API_QUERY = base+'/user/getRoles';//根据用户id查找角色
+    var API_FETCH = base +'/role/query';//查询角色管理
+    var API_DESTROY = base+'/role/delete';//删除角色管理
+    var Model = Backbone.Model.extend({
+        idAttribute: 'id',
+        edit: function(data){
+            var model = this;
+            data = _.extend({id:model.id},data);
+            return Entities.sync(API_EDIT,data).then(function(res){
+                model.set(_.extend(data,res));
+            });
+        },
+        delete: function(){
+            var model = this;
+            var data = {id:model.id};
+            return Entities.sync(API_DESTROY,data).then(function(res){
+                model.trigger('destroy', model, model.collection,{removeself:true});
+            });
+        },
+    });
+
+    var Collection = Backbone.Collection.extend({
+        model:Model,
+        fetch: function(data){
+            var collection=this;
+            return Entities.sync(API_FETCH,data).then(function(res){
+                collection.reset(res);
+            });
+        },
+        query:function(data){
+            console.log(data);
+            var collection=this;
+            return Entities.sync(API_QUERY,data).then(function(res){
+                collection.reset(res);
+            });
+        },
+        create: function(data){
+            var collection=this;
+            return Entities.sync(API_SAVE,data).then(function(res){
+                collection.unshift(_.extend(data,res));
+            });
+        }
+    });
+    return {
+        Model: Model,
+        Collection: Collection
+    };
+}(Backbone,Entities,_));
