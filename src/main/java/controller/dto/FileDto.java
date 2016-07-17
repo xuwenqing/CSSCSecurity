1573a66a4c78cadab20f8d6dcc61cc5d9cdadbf9
xuwenqing <qing18363031040>
2016-07-17 20:52:08 +0800

    大文件上传

---
+++ src/main/java/controller/dto/FileDto.java
@@ -0,0 +1,52 @@
+package controller.dto;
+
+/**
+ * Created by wenqing on 2016/7/17.
+ */
+public class FileDto {
+    private String filename;
+    private String filepath;
+    private String ext;
+    private String size;
+    private String md5;
+
+    public String getMd5() {
+        return md5;
+    }
+
+    public void setMd5(String md5) {
+        this.md5 = md5;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getExt() {
+        return ext;
+    }
+
+    public void setExt(String ext) {
+        this.ext = ext;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getFilepath() {
+        return filepath;
+    }
+
+    public void setFilepath(String filepath) {
+        this.filepath = filepath;
+    }
+}
