64ff2cbd0996abda660365c352bfefea2c22ace5
xuwenqing <qing18363031040>
2016-08-02 11:55:38 +0800

    登陆

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,7 +1,9 @@
 package controller;
 
+import controller.dto.ResponsePackDto;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +18,8 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class LoginController extends BaseController {
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String showLoginPage() {
-        return "login";
-    }
-
     @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public String loginFailed(HttpServletRequest req, Model model) {
+    public @ResponseBody ResponsePackDto  loginFailed(HttpServletRequest req) {
         String exceptionClassName = (String)req.getAttribute("shiroLoginFailure");
         String error = null;
         if(UnknownAccountException.class.getName().equals(exceptionClassName)) {
@@ -32,7 +29,16 @@ public class LoginController extends BaseController {
         } else if(exceptionClassName != null) {
             error = "其他错误：" + exceptionClassName;
         }
-        model.addAttribute("error", error);
-        return "login";
+        ResponsePackDto dto = new ResponsePackDto(error);
+        dto.setStatus(500);
+        return dto;
     }
+
+    @RequiresAuthentication
+    @RequestMapping("/login/success")
+    public @ResponseBody
+    ResponsePackDto index(HttpServletRequest req, Model model) {
+        return new ResponsePackDto();
+    }
+
 }
